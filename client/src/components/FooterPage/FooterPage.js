aa45ae51c6881df35c0147b48c9e2d7513c70cbc
Vger <vcdelariva@gmail.com>
2019-01-14 10:43:54 -0600

    Consistent use of npm for bootstrap-react

--- client/src/components/FooterPage/FooterPage.js
+++ client/src/components/FooterPage/FooterPage.js
@@ -1,21 +1,21 @@
 import React from "react";
 import "./FooterPage.css";
-import { Col, Container, Row, Footer } from "mdbreact";
+import { Col, Row } from "react-bootstrap";
 import MediaQuery from 'react-responsive';
 
 const FooterPage = () => (
   
-<Footer className="font-small pt-4 mt-4">
-  <Container fluid className="text-md-left footercontent">
+<div className="font-small pt-4 mt-4">
+  <div fluid="true" className="text-md-left footercontent">
     <Row>
-      <Col md="4" className="title">
+      <Col md={4} className="title">
       <h3>Vote Now</h3>
       <p>
       VOTE NOW is an application to encourage people to actively engage and participate in the elections by providing information about where to vote and a list of their candidates. VOTE NOW's helpful resources include podcasts and links to a variety of resources that allows them to make a educated and informed decision.
       </p>
       </Col>
 
-      <Col md="4">
+      <Col md={4}>
       <h3 className="title resources">Helpful Resources</h3>
         <h4 className="list-unstyled resources">
           <a href="https://www.rockthevote.org/">Rock the Vote</a>
@@ -34,20 +34,20 @@ const FooterPage = () => (
         </h4>
       </Col>
 
-      <Col md="4">
+      <Col md={4}>
         <MediaQuery minWidth={1024}>
          <img src="../../logo.png" alt="Vote Now" className="footerlogo" />
         </MediaQuery>
       </Col>
 
     </Row>
-  </Container>
-  <div className="footer-copyright text-center py-3">
-    <Container fluid>
-      VOTE NOW Team Project 3  |  <a href="https://github.com/vdelariva/voteNow">Github <i className="fab fa-github-square"></i></a>
-    </Container>
   </div>
-</Footer>
+  <div className="footer-copyright text-center py-3">
+    <div fluid="true">
+      VOTE NOW Team Project 3  |  <a href="https://github.com/vdelariva/voteNow">Github <i className="fab fa-github-square"></i></a>
+    </div>
+  </div>
+</div>
 );
 
 export default FooterPage;
